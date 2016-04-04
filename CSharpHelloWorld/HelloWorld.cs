1e87fad83f14b2dfe7a0494f70b0e2c8c75a3737
Nicolas LAVOISIER <nlavoisier@bk-consulting.com>
2016-04-04 17:48:11 +0200

    Initialize hello world project

---
+++ CSharpHelloWorld/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHelloWorld
+{
+    public class HelloWorld
+    {
+        public const string message = "Hello World";
+
+        public void sayHello()
+        {
+            Console.WriteLine(message);
+        }
+
+        public void sayHelloFrom(String Name)
+        {
+            Console.WriteLine(message + " from " + Name);
+        }
+    }
+}
---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpHelloWorld;
+using System.IO;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            // put back console output to default state
+            StreamWriter standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            Console.SetOut(standardOut);
+        }
+
+        [TestMethod]
+        public void shouldSayHelloWriteHelloWorldInConsole()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld hello = new HelloWorld();
+                hello.sayHello();
+
+                string expected = "Hello World";
+                string result = sw.ToString();
+                Assert.AreEqual<string>(expected, result);
+
+            }
+        }
+    }
+}
